f3656d85ef93d0ecce260b5f1a5e476e2563946b
Shay Cohen <shaycohen2003@gmail.com>
2023-07-11 12:12:34 +0300

    Update Exploration.sql

--- Exploration.sql
+++ Exploration.sql
@@ -13,9 +13,11 @@ WHERE EventType = 'CaseSolved';
 
 #####################################################################################################
 
-## Finding the caseid which have been solved at the last transaction - by adding the max over the time_stamp 
 
-SELECT CaseId
+# Finding the caseid which have been solved at the last transaction - by adding the max over the time_stamp 
+# In other words, the last transaction recorded for that case is 'CaseSolved'.
+
+SELECT CaseId 
 FROM DetectiveCases
 WHERE EventType = 'CaseSolved'
 GROUP BY CaseId
@@ -23,4 +25,18 @@ HAVING MAX(Time_stamp) = (SELECT MAX(Time_stamp)
 						  FROM DetectiveCases
 						  WHERE EventType = 'CaseSolved'
 						  GROUP BY CaseId);
+                      
+
+### Here I used the same subquery I wrote -The only I added  is the output "DetectiveId"
+                          
+SELECT CaseId, DetectiveId
+FROM DetectiveCases
+WHERE EventType = 'CaseSolved'
+GROUP BY CaseId
+HAVING MAX(Time_stamp) = (SELECT MAX(Time_stamp)
+						  FROM DetectiveCases
+						  WHERE EventType = 'CaseSolved'
+						  GROUP BY Case_id);
+
+
                           
