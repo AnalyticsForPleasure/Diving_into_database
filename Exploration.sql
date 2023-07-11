1797d2d43c5a5aa4ed14cae1b723dd84e09ce9cf
Shay Cohen <shaycohen2003@gmail.com>
2023-07-11 14:01:17 +0300

    Update Exploration.sql

--- Exploration.sql
+++ Exploration.sql
@@ -40,5 +40,17 @@ HAVING MAX(Time_stamp) = (SELECT MAX(Time_stamp)
 						  GROUP BY CaseId);
 
 
+# Let's to for a different approach - Using join + nested subqeury
+	
+SELECT DetectiveC.CaseId, DetectiveC.DetectiveId
+FROM DetectiveCases DetectiveC
+JOIN (SELECT CaseId, MAX(Time_stamp) AS max_timestamp
+	  FROM DetectiveCases
+	  WHERE EventType = 'CaseSolved'
+	  GROUP BY CaseId) sub 
+ON DetectiveC.CaseId = sub.CaseId AND
+ DetectiveC.Time_stamp = sub.max_timestamp # This line is used  - In order to find the last line of the time_stamp for a specific CaseId
+WHERE DetectiveC.EventType = 'CaseSolved'; # The 'WHERE' here is a continuation for the outer query 
+
 
                           
