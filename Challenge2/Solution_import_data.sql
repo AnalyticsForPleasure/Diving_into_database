88d4360c2ad1ba7a9209e16a5c73b641501eea8a
Shay Cohen <shaycohen2003@gmail.com>
2023-08-16 16:37:35 +0300

    Update Solution_import_data.sql

--- Challenge2/Solution_import_data.sql
+++ Challenge2/Solution_import_data.sql
@@ -11,7 +11,7 @@ use Kusto_Detective_season2;
 show variables like "secure_file_priv";
 show variables like "local_infile";
 
-drop table consumption_df;
+
 
 -- Dealing with the first table of the detective kusto challenge -  "consumption_df" :
 CREATE TABLE consumption_df
@@ -23,22 +23,26 @@ Time_stamp datetime,
 );
 
 
-drop table cost_df;
+
 
 -- Dealing with the second table of the detective kusto challenge -  "Reference table" :
 CREATE TABLE cost_df
 (
-MeterType TEXT,
+    MeterType TEXT,
     Unit TEXT,
     Cost double
 );
 
-INSERT INTO cost_df
-(MeterType, Unit, Cost)
+INSERT INTO cost_df (MeterType, Unit, Cost)
 Values ('Water','Liter','0.001562'),  
   ('Electricity','kwH','0.3016');
 
+drop table consumption_df;
+drop table cost_df;
 
 
 select *
 from consumption_df;
+
+select *
+from cost_df;
