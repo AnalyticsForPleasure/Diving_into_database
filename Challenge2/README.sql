d0b49a65781c37ded1158e31c133fcd3204ed3d8
Shay Cohen <shaycohen2003@gmail.com>
2023-08-18 11:53:49 +0300

    Create README.sql

---
+++ Challenge2/README.sql
@@ -0,0 +1,10 @@
+<p align="center">
+<img width="980" src="images/Mysql_logo_readme.png" title="bla">
+</p>
+
+- [Introduction](#introduction)
+- [Mystery Question](#mystery-question)
+- [Answer](#answer)
+  - [Part 1 : Importing data](#part-1--importing-data)
+  - [Part 2 : Exploration detective mystery](#part-2--exploration-detective-mystery)
+
